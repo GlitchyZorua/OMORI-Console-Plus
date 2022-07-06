17b1c88429a86023451e141f882c23129cf740d9
Jacobwworkman <54043124+Jacobw0@users.noreply.github.com>
2022-07-05 17:00:10 -0800

    aaa

---
+++ src/about.js
@@ -0,0 +1,7 @@
+if (typeof window.commands !== "undefined") {
+
+    const aboutOnCommand = (handler, args) => {
+         handler.log("Copyleft – 🄯 -- All wrongs reversed. 2022\n\n\nOriginal project By surrealegg.\n\nConsole Plus was created by Jacobw.\nOMORI Created By OMOCAT.\nMade with love.\n(Mod not created by omocat)")
+  };
+    window.commands.add("about", aboutOnCommand);
+}
