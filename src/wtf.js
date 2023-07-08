b06358bb7c9d670d2f0c3469a08856d1d233590c
Jacobw0 <54043124+GlitchyZorua@users.noreply.github.com>
2023-07-08 13:38:55 -0700

    fixed the fucking wtf command finally

--- src/wtf.js
+++ src/wtf.js
@@ -3,8 +3,15 @@ if (typeof window.commands !== "undefined") {
 
     const wtfOnCommand = (handler, args) => {
         if (args.length < 2) {
-            handler.log("WTF level is at "+args[1]+"\n\nUsage: /wtf [#]");
+            handler.log("WTF level is at "+ $gameVariables.value(143) +"\n\nUsage: /wtf [#]");
             return;
+        }
+        if(isNaN(args[1])){
+            handler.error('Not a number!');
+            return;
+        }
+        if(args[1] == 143){
+            handler.log('I love you too! <3'); // don't ask.
         }
         handler.log("WTF Value is set to " +args[1])
         $gameVariables.setValue(143, args[1])
