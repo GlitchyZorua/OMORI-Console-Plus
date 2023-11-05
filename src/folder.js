b0d67811af5d1680076d5e698e1672a9586381e7
Jacobw0 <54043124+GlitchyZorua@users.noreply.github.com>
2023-11-05 13:10:59 -0800

    FINE.

--- src/folder.js
+++ src/folder.js
@@ -1,5 +1,5 @@
 
-let linuxpath = "/home/jacobw/.local/share/Steam/steamapps/common"; // for fuck sakes i don't understand why this will never work on linux. Probably because of wine or something, i don't know
+// let linuxpath = "/home//.local/share/Steam/steamapps/common"; // for fuck sakes i don't understand why this will never work on linux. Probably because of wine or something, i don't know
 let windowspath = "C:/Program Files/Steam/SteamApps/common/OMORI"; 
 var gui = require('nw.gui'); 
 
@@ -7,7 +7,8 @@ var gui = require('nw.gui');
 if (typeof window.commands !== "undefined") {
     const folderOnCommand = (args) => {
       if (args[1] == "-l"){
-        gui.Shell.openExternal(linuxpath); //linux
+        handler.error('This argument is disabled. Blame my crappy coding.');
+        //gui.Shell.openExternal(linuxpath); //linux
         return;
       } else {
         gui.Shell.openExternal(windowspath); //windows
