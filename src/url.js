593c540054a31f1a6895a2deff04a5e47d640a92
Jacobw0 <54043124+GlitchyZorua@users.noreply.github.com>
2023-12-24 16:05:05 -0800

    Update url.js

--- src/url.js
+++ src/url.js
@@ -1,6 +1,6 @@
 if (typeof window.commands !== "undefined") {
   const urlOnCommand = (args) => {
-    if (confirm("Please save all of your data and press OK.") == true) { // todo: use omori text boxes to confirm, that is, if i can.
+    if (confirm("Please save all of your data and press OK.\n\n\nBY USING THIS COMMAND, YOU ARE RESPONSIBLE FOR WHAT YOU DONE THROUGH THIS MOD. IF YOU GET HACKED BECAUSE OF THIS MOD, YOU ARE STUPID.") == true) { // todo: use omori text boxes to confirm, that is, if i can.
       window.location.replace(args[1]); // man fucking hate it when people put alert() in there omori mods. but whatever. it works.
     } else {
     return; 
